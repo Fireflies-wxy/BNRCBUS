fcf60ca95491319d24b42c940cf45e6230730d9e
fireflies <562802717@qq.com>
2017-11-30 11:37:12 +0800

    second push

--- bNRCBUS/src/main/java/com/bnrc/busapp/ArActivity.java
+++ bNRCBUS/src/main/java/com/bnrc/busapp/ArActivity.java
@@ -2,23 +2,53 @@ package com.bnrc.busapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bnrc.util.BusTags;
+import com.bnrc.util.CameraSurfaceView;
 import com.bnrc.util.collectwifi.BaseActivity;
 
-public class ArActivity extends BaseActivity {
+public class ArActivity extends BaseActivity implements View.OnClickListener,BusTags.IAutoFocus{
 
-    private ImageView picture;
+    private CameraSurfaceView mCameraSurfaceView;
+    private BusTags mBusTags;
+
+    private boolean isClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ar);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 全屏显示
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        mCameraSurfaceView = (CameraSurfaceView) findViewById(R.id.cameraSurfaceView);
+        mBusTags = (BusTags) findViewById(R.id.busTags);
+        mBusTags.setIAutoFocus(this);
+        mBusTags.setOnClickListener(this);
 
-        picture = (ImageView) findViewById(R.id.picture);
+    }
 
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivityForResult(intent,1);
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.busTags:
+                Toast.makeText(ArActivity.this,"You clicked a tag", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void autoFocus() {
+        mCameraSurfaceView.setAutoFocus();
     }
 }
